211efe360f44f7bf70cd038a8cfde90d4de3b1fb
Alexander Smith <acsmith.dev@gmail.com>
2019-07-06 11:57:53 -0400

    update list and create methods in server

--- server/src/app.js
+++ server/src/app.js
@@ -21,14 +21,35 @@ db.once("open", function(callback){
 // Models
 var Post = require("../models/post");
 
-// Endpoints
+// GET - /posts
 app.get('/posts', (req, res) => {
-  res.send(
-    [{
-      title: "Hello World!",
-      description: "Hi there! How are you?"
-    }]
-  )
+  Post.find({}, 'title description', function (error, posts) {
+    if (error) { console.error(error); }
+    res.send({
+      posts: posts
+    })
+  }).sort({_id:-1})
+})
+
+// POST - /posts
+app.post('/posts', (req, res) => {
+  var db = req.db;
+  var title = req.body.title;
+  var description = req.body.description;
+  var new_post = new Post({
+    title: title,
+    description: description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully!'
+    })
+  })
 })
 
 app.listen(process.env.PORT || 8081)
